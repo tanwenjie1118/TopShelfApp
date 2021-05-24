fe750107d45166cefd345812232b05e8621e656c
hal_t <hal_t@DESKTOP-C46QC8G>
2021-05-24 17:43:28 +0800

    first commit

---
+++ TopShelfDemo/Jobs/SpecialService.cs
@@ -0,0 +1,77 @@
+﻿using Quartz;
+using Quartz.Impl;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TopShelfDemo.Jobs
+{
+    public class SpecialService
+    {
+        private IScheduler scheduler;
+        public SpecialService()
+        {
+            if (scheduler == null)
+            {
+                // Grab the Scheduler instance from the Factory
+                StdSchedulerFactory factory = new StdSchedulerFactory();
+                scheduler = factory.GetScheduler().Result;
+            }
+        }
+        public async Task Start()
+        {
+            await RunJobs();
+        }
+        public async Task Stop()
+        {
+            //To do something
+            await scheduler.Shutdown();
+        }
+
+        private async Task RunJobs()
+        {
+            var builder = JobBuilder.Create<JobConsole>();
+            var job = builder.Build();
+
+            var triggerBuilder = TriggerBuilder.Create();
+            triggerBuilder.WithCronSchedule("0/10 * * * * ?");
+            var trigger = triggerBuilder.Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+
+            // and start it off
+            await scheduler.Start();
+
+            // some sleep to show what's happening
+            //await Task.Delay(TimeSpan.FromSeconds(10));
+
+            // and last shut down the scheduler when you are ready to close your program
+            //await scheduler.Shutdown();
+        }
+
+        private void CallApi(string url)
+        {
+            IRestClient client = new RestClient
+            {
+                BaseUrl = new Uri(url)
+            };
+            IRestRequest request = new RestRequest();
+
+            client.Post(request);
+        }
+
+        private class JobConsole : IJob
+        {
+            public async Task Execute(IJobExecutionContext context)
+            {
+                // Console.WriteLine("Now is time :" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ok.txt");
+                File.AppendAllLines(path, new List<string> { "This is time : " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") }); ;
+            }
+        }
+    }
+}
---
+++ TopShelfDemo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using Topshelf;
+using TopShelfDemo.Jobs;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            RunTopShelfWinService();
+        }
+
+        private static async Task RunConsoleJob()
+        {
+            try
+            {
+                var service = new SpecialService();
+                await service.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.Read();
+        }
+
+        private static void RunTopShelfWinService()
+        {
+            // configurating service factory
+            HostFactory.Run(x =>
+            {
+                x.Service<SpecialService>(s =>
+                {
+                    // services want to run
+                    s.ConstructUsing(name => new SpecialService());
+
+                    // when service started
+                    s.WhenStarted(async tc =>await tc.Start());
+
+                    // when service stopped
+                    s.WhenStopped(async tc => await tc.Stop());
+                });
+
+                // How to run
+                x.RunAsLocalSystem();
+
+                // Description
+                x.SetDescription("A demo of TopShelf application");
+
+                // Display name
+                x.SetDisplayName("TopShelfJobService");
+
+                // Service name
+                x.SetServiceName("TopShelfJobService");
+            });
+        }
+    }
+}
